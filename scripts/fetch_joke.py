b0f15e52cd14dafc498c0a5efeea23e44eccc65d
fy <fidel.cacheda@udc.es>
2026-02-05 13:56:31 +0100

    Update fetch_joke.py

--- scripts/fetch_joke.py
+++ scripts/fetch_joke.py
@@ -2,7 +2,13 @@ import json
 import urllib.request
 
 URL = "https://icanhazdadjoke.com/"
-req = urllib.request.Request(URL, headers={"Accept": "application/json"})
+req = urllib.request.Request(
+    URL,
+    headers={
+        "Accept": "application/json",
+        "User-Agent": "PracticasRedes (https://github.com/USUARIO/REPO)"
+    }
+)
 
 with urllib.request.urlopen(req, timeout=10) as r:
     data = json.loads(r.read().decode("utf-8"))
